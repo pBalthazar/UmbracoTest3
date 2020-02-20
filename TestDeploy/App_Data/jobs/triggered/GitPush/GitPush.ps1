ce36cfe512ceedcbd8308228ff5ed781f9994033
Philippe Balthazar <pbalthazar@nmedia.ca>
2020-02-20 14:22:08 -0500

    Ajout des répertoires scripts et views dans le push

--- TestDeploy/App_Data/jobs/triggered/GitPush/GitPush.ps1
+++ TestDeploy/App_Data/jobs/triggered/GitPush/GitPush.ps1
@@ -1,5 +1,7 @@
 Copy-Item D:\home\site\wwwroot\Media -Destination "D:\home\site\UmbracoTest3\TestDeploy\" -Recurse -force
 Copy-Item D:\home\site\wwwroot\uSync -Destination "D:\home\site\UmbracoTest3\TestDeploy\" -Recurse -force
+Copy-Item D:\home\site\wwwroot\scripts -Destination "D:\home\site\UmbracoTest3\TestDeploy\" -Recurse -force
+Copy-Item D:\home\site\wwwroot\Views -Destination "D:\home\site\UmbracoTest3\TestDeploy\" -Recurse -force
 
 git -C D:\home\site\UmbracoTest3 add -A
 git -C D:\home\site\UmbracoTest3 commit -m "Autopush webJob"
